9562315d7a625660ecf5442f8dcb3f521792963c
prettob <bernardodgpretto@gmail.com>
2021-02-24 17:41:39 -0300

    first commit

---
+++ src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { SurveysController } from "./controllers/SurveysController";
+import { UsersController } from "./controllers/UsersController";
+
+const router = Router();
+
+const usersController = new UsersController();
+const surveysController = new SurveysController();
+
+router.post("/users", usersController.create)
+router.post("/surveys", surveysController.create)
+router.get("/surveys", surveysController.show)
+
+export { router };
